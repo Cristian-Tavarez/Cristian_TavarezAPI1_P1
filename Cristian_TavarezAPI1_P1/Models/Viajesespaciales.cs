b65bbebbccfc7a11f3f47edba3dfb865f8b2441b
cristian tavarez <cristian22041@gmail.com>
2026-02-02 19:58:57 -0400

    Creando la Carpeta Models con su clase Viajesespaciales.cs

---
+++ Cristian_TavarezAPI1_P1/Models/Viajesespaciales.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cristian_TavarezAPI1_P1.Models
+{
+    public class Viajeespacial 
+    {
+        [Key]
+        public int ViajeId { get; set; }
+
+        [Required]
+        public string FechaId { get; set; }
+
+        [Required]
+        public string Descripcion { get; set; }
+
+    }
+}
