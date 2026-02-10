dac0db2730a132166cdc2c2f1c9f544780b09174
cristian tavarez <cristian22041@gmail.com>
2026-02-09 21:08:29 -0400

    Creando el EntradasHuacalesContexto.cs

---
+++ Cristian_TavarezAPI1_P1/Context/EntradasHuacalesContexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cristian_TavarezAPI1_P1.Models;
+
+namespace Cristian_TavarezAPI1_P1.Contexto;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<EntradaHuacales> EntradasHuacales { get; set; }
+}
+
+
+
