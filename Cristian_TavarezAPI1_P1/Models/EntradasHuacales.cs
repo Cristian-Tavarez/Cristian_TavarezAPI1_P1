d8d11b411030a4c3de59e614a0221d811c647726
Administrator <Administrator@WIN-RNEVULAN3K9>
2026-02-19 18:51:59 -0800

    Agregar archivos de proyecto.

---
+++ Cristian_TavarezAPI1_P1/Models/EntradasHuacales.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cristian_TavarezAPI1_P1.Models;
+
+public class EntradaHuacales
+{
+    [Key]
+    public int IdEntrada { get; set; }
+
+    [Required]
+    public DateTime Fecha { get; set; } = DateTime.Today;
+
+    [Required]
+    public string NombreCliente { get; set; } = "";
+
+    [Required]
+    public int Cantidad { get; set; }
+
+    [Required]
+    public decimal Precio { get; set; }
+}
+ 
+
+
---
+++ Cristian_TavarezAPI1_P1/Program.cs
@@ -0,0 +1,35 @@
+using Cristian_TavarezAPI1_P1.Components;
+using Cristian_TavarezAPI1_P1.Services;
+using Cristian_TavarezAPI1_P1.Contexto;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<EntradasHuacalesService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
