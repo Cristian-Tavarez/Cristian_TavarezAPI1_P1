1ad318148ee45cf36747072c65ffa9ade4c9c5b9
cristian tavarez <cristian22041@gmail.com>
2026-02-28 02:20:17 -0800

    Creando EntradaHuacalDetalle.cs

---
+++ Cristian_TavarezAPI1_P1/Models/EntradaHuacalDetalle.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cristian_TavarezAPI1_P1.Models
+{
+    public class EntradaHuacalDetalle
+    {
+        public int Id { get; set; }
+
+        public int EntradaId { get; set; }
+        public EntradaHuacales? Entrada { get; set; }
+
+        public int TipoId { get; set; }
+        public TipoHuacal? TipoHuacal { get; set; }
+
+        public int Cantidad { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Precio { get; set; }
+    }
+}
