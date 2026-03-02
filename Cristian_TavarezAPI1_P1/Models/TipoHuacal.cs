e5f0f0c8f4ff63531238102d59b30cb5463eb975
cristian tavarez <cristian22041@gmail.com>
2026-03-01 16:53:28 -0800

    Creacion de TipoHuacal.cs

---
+++ Cristian_TavarezAPI1_P1/Models/TipoHuacal.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cristian_TavarezAPI1_P1.Models
+{
+    public class TipoHuacal
+    {
+        [Key]
+        public int TipoId { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public int Existencia { get; set; }
+
+        public ICollection<EntradaHuacalDetalle>? Detalles { get; set; }
+    }
+}
