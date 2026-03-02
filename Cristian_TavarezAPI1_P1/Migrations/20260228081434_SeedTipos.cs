282886eb492764f6c7eb138563eeb3f50b1e9d16
cristian tavarez <cristian22041@gmail.com>
2026-03-01 17:12:19 -0800

    Creando Migracion Tipos

---
+++ Cristian_TavarezAPI1_P1/Migrations/20260228081434_SeedTipos.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Cristian_TavarezAPI1_P1.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTipos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TiposHuacales",
+                columns: new[] { "TipoId", "Descripcion", "Existencia" },
+                values: new object[,]
+                {
+                    { 1, "Rojo", 0 },
+                    { 2, "Verde", 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TiposHuacales",
+                keyColumn: "TipoId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TiposHuacales",
+                keyColumn: "TipoId",
+                keyValue: 2);
+        }
+    }
+}
