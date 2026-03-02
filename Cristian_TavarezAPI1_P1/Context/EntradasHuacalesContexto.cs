bc5cb8baaccfb0493c414972ddfe14e0ce45feb0
cristian tavarez <cristian22041@gmail.com>
2026-03-01 16:51:23 -0800

    Modificando EntradasHuacalesContexto.cs

--- Cristian_TavarezAPI1_P1/Context/EntradasHuacalesContexto.cs
+++ Cristian_TavarezAPI1_P1/Context/EntradasHuacalesContexto.cs
@@ -9,6 +9,28 @@ public class AppDbContext : DbContext
         : base(options) { }
 
     public DbSet<EntradaHuacales> EntradasHuacales { get; set; }
+    public DbSet<TipoHuacal> TiposHuacales { get; set; }
+    public DbSet<EntradaHuacalDetalle> EntradasHuacalesDetalle { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<EntradaHuacalDetalle>()
+            .HasOne(d => d.Entrada)
+            .WithMany(e => e.Detalles)
+            .HasForeignKey(d => d.EntradaId);
+
+        modelBuilder.Entity<EntradaHuacalDetalle>()
+            .HasOne(d => d.TipoHuacal)
+            .WithMany(t => t.Detalles)
+            .HasForeignKey(d => d.TipoId);
+
+        modelBuilder.Entity<TipoHuacal>().HasData(
+       new TipoHuacal { TipoId = 1, Descripcion = "Rojo", Existencia = 0 },
+       new TipoHuacal { TipoId = 2, Descripcion = "Verde", Existencia = 0 }
+       );
+    }
 }
 
 
